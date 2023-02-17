b6a0d6a7bbc582328f2a5abee362d6e38b052ad7
kazeem <yusufkazeem71@gmail.com>
2023-02-17 12:55:30 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main funtion
+ *
+ * Return: always 0 when successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
